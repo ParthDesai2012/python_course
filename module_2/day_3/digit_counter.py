8fc5f01ccb857538da40bb09b52d7df8f5f405a6
ParthDesai2012 <parthdesai863@gmail.com>
2025-06-29 14:26:34 +0400

    Submited acp for day 3

--- module_2/day_3/digit_counter.py
+++ module_2/day_3/digit_counter.py
@@ -1,2 +1,6 @@
-number = input("Enter a number: ")
-print("Total number of digits:", len(number))
+num = input("Enter a number: ")
+count = 0
+for i in range(len(num)):
+    if num[i].isdigit():
+        count += 1
+print("Total number of digits:", count)
